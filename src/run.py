85c5665f18c362fa426927ed532a063bc1989c54
David Feinblum <dvfeinblum@gmail.com>
2018-09-23 14:22:35 -0400

    Store links in cache so we can skip a GET

--- src/run.py
+++ src/run.py
@@ -5,20 +5,29 @@ from requests import get
 
 BLOG_FEED_URL = 'http://avagadbro.blogspot.com/feeds/posts/default'
 
-# Some useful regex
+# Some useful regex for parsing links
 POST_PREFIX = "^http://avagadbro.blogspot.com/2"
 REL = "^alternate"
 
+# Redis setup
 client = StrictRedis()
+LINKS_KEY = 'blog_links'
 
 
 def get_blogpost_links():
-    blog_contents = get(BLOG_FEED_URL).content
-    soup = BeautifulSoup(blog_contents,
-                         features="html.parser")
-    return [link.attrs['href'] for link in soup.findAll('link', attrs={'href': re.compile(POST_PREFIX),
-                                                                       'rel': re.compile(REL)})]
+    soup = BeautifulSoup(get(BLOG_FEED_URL).content,
+                         features='html.parser')
+    links = [link.attrs['href'] for link in soup.findAll('link', attrs={'href': re.compile(POST_PREFIX),
+                                                                        'rel': re.compile(REL)})]
+
+    client.set(LINKS_KEY, links)
+    return links
 
 
 if __name__ == "__main__":
-    print(get_blogpost_links())
+    blog_links = client.get(LINKS_KEY)
+    if blog_links is None:
+        print('Link cache is currently empty. Scraping blog feed at {}'.format(BLOG_FEED_URL))
+        blog_links = get_blogpost_links()
+
+    print(blog_links)
