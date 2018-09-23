9990e3ab3c268bd7d02984293f0df7a4ee9a9d4f
David Feinblum <dvfeinblum@gmail.com>
2018-09-23 18:41:01 -0400

    Pulled out entry-content and added some docstrings

--- src/run.py
+++ src/run.py
@@ -6,9 +6,10 @@ from requests import get
 
 BLOG_FEED_URL = 'http://avagadbro.blogspot.com/feeds/posts/default'
 
-# Some useful regex for parsing links
-POST_PREFIX = "^http://avagadbro.blogspot.com/2"
-REL = "^alternate"
+# Some useful constants for parsing blog html
+POST_PREFIX_REGEX = "^http://avagadbro.blogspot.com/2"
+POST_URL_REL = "alternate"
+POST_BODY_CLASS = 'post-body entry-content'
 
 # Redis setup
 client = StrictRedis()
@@ -16,18 +17,29 @@ LINKS_KEY = 'blog_links'
 
 
 def get_blogpost_links():
+    """
+    This goes to the RSS for the blog and fetches a URL from each post
+    :return: Array of URLs
+    """
     soup = BeautifulSoup(get(BLOG_FEED_URL).content,
                          features='html.parser')
-    links = [link.attrs['href'] for link in soup.findAll('link', attrs={'href': re.compile(POST_PREFIX),
-                                                                        'rel': re.compile(REL)})]
+    links = [l.attrs['href'] for l in soup.findAll('link', attrs={'href': re.compile(POST_PREFIX_REGEX),
+                                                                  'rel': POST_URL_REL})]
 
     client.set(LINKS_KEY, links)
+
     return links
 
 
 def parse_blog_post(blog_link):
-    resp = get(blog_link)
-    print(resp.content)
+    """
+    Given a blog post's URL, this function GETs it and pulls the body out
+    :param blog_link: String
+    :return: Raw text from the blog post w/html tags
+    """
+    soup = BeautifulSoup(get(blog_link).content,
+                         features='html.parser')
+    return soup.find('div', attrs={'class': POST_BODY_CLASS})
 
 
 if __name__ == "__main__":
@@ -40,4 +52,4 @@ if __name__ == "__main__":
         blog_links = ast.literal_eval(blog_links.decode('utf-8'))
 
     for link in blog_links:
-        parse_blog_post(link)
+        print(parse_blog_post(link))
