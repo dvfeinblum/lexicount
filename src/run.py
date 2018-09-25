db598381c12cea2fc208a383dadf94c861dcb93f
David Feinblum <dvfeinblum@gmail.com>
2018-09-24 21:43:59 -0400

    Calculating some stats.

--- src/run.py
+++ src/run.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 from redis import StrictRedis
 from requests import get
+import string
 
 BLOG_FEED_URL = 'http://avagadbro.blogspot.com/feeds/posts/default'
 
@@ -11,9 +12,11 @@ POST_PREFIX_REGEX = "^http://avagadbro.blogspot.com/2"
 POST_URL_REL = "alternate"
 POST_BODY_CLASS = 'post-body entry-content'
 
-# Redis setup
+# Redis stuff
 client = StrictRedis()
 LINKS_KEY = 'blog_links'
+blogs_scraped_counter = 0
+word_count = 0
 
 
 def get_blogpost_links():
@@ -35,11 +38,39 @@ def parse_blog_post(blog_link):
     """
     Given a blog post's URL, this function GETs it and pulls the body out
     :param blog_link: String
-    :return: Raw text from the blog post w/html tags
+    :return: Raw text from the blog post w/html tags stripped out
     """
+    global blogs_scraped_counter
+    global word_count
+    print('Fetching raw text for {}'.format(blog_link))
     soup = BeautifulSoup(get(blog_link).content,
                          features='html.parser')
-    return soup.find('div', attrs={'class': POST_BODY_CLASS})
+    post_text = soup.find('div', attrs={'class': POST_BODY_CLASS}).get_text()
+
+    # To remove punctuation, we use a translator
+    translator = str.maketrans('', '', string.punctuation)
+
+    print('Successfully parsed post. Updating word count in redis.')
+    for word in post_text.replace('\n', ' ').translate(translator).split(' '):
+        client.incr(word)
+        word_count = word_count + 1
+    blogs_scraped_counter = blogs_scraped_counter + 1
+
+
+def get_results():
+    """
+    Once the run is complete, we'll spit out some stats.
+    :return:
+    """
+    # we subtract one because of the blog_links entry
+    unique_word_count = client.dbsize() - 1
+    print('\nRESULTS\n')
+    print('Number of words found across all posts: {}'.format(word_count))
+    print('Number of unique words found across all posts: {}'.format(unique_word_count))
+    print('Number of posts scraped: {}\n'.format(blogs_scraped_counter))
+    print('Average repeat-rate of all words: {}'.format(word_count / unique_word_count))
+    print('Average words per post: {}'.format(word_count / blogs_scraped_counter))
+    print('Unique words per post: {}\n'.format(unique_word_count / blogs_scraped_counter))
 
 
 if __name__ == "__main__":
@@ -52,4 +83,6 @@ if __name__ == "__main__":
         blog_links = ast.literal_eval(blog_links.decode('utf-8'))
 
     for link in blog_links:
-        print(parse_blog_post(link))
+        parse_blog_post(link)
+
+    get_results()
